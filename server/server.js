fc0a0c123d5ab54a8a0537b43b902db3d01bbe2d
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-22 23:52:01 -0500

    setup concurrently to run both servers

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const path = require('path');
+
 const { authMiddleware } = require('./utils/auth');
 
 const startServer = async () => {
@@ -34,6 +36,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
 	app.listen(PORT, () => {
