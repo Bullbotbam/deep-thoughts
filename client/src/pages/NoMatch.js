d810d9c7ac788fe0c7e4e9e052f348c33147a87f
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-24 17:42:41 -0500

    JWT set up to provide logged in users different experience than those that are not logged in

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const NoMatch = () => {
-  return (
-    <div>
-      Oops, we couldn't find that page.
-    </div>
-  );
+	return (
+		<div>
+			<h1>404</h1>
+			Oops, we couldn't find that page.
+		</div>
+	);
 };
 
 export default NoMatch;
